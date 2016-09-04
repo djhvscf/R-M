95f04240a81f159caad9e09a6d2d3e96ecd932e7
Dennis Hernández <dennishernandezvargas@gmail.com>
2016-09-04 02:25:21 -0600

    Adding a sample app

---
+++ Sport-master/Sport.Service/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sport.Service
+{
+	internal class Constants
+	{
+		internal static readonly string HubConnectionString = "Endpoint=sb://xamarinsporthub-ns.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=fJ3cZPdYVh+KFuUSzXyybBiaZLIgzZ9qfP38cJ4mIMk=";
+		internal static readonly string HubName = "xamarinsporthub2";
+	}
+}
---
+++ Sport-master/Sport.Shared/ViewModels/Admin/AdminViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Xamarin.Forms;
+
+namespace Sport.Shared
+{
+	public class AdminViewModel : BaseViewModel
+	{
+	}
+}
