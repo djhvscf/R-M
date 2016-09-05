0c099e5f5547ac118abd2944cc650fdafef358be
Dennis Hernández <dennishernandezvargas@gmail.com>
2016-09-05 00:03:06 -0600

    Adding android app

---
+++ MessageSender/MessageSender.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace MessageSender
+{
+    class MessageSender
+    {
+        public const string API_KEY = "AIzaSyAGnE4Z8p658N0snwEKDecrEFWwq2vesZ4";
+        public const string MESSAGE = "Hello, Xamarin!";
+
+        static void Main(string[] args)
+        {
+            var jGcmData = new JObject();
+            var jData = new JObject();
+
+            jData.Add("message", MESSAGE);
+            jGcmData.Add("to", "/topics/global");
+            jGcmData.Add("data", jData);
+
+            var url = new Uri("https://gcm-http.googleapis.com/gcm/send");
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    client.DefaultRequestHeaders.Accept.Add(
+                        new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    client.DefaultRequestHeaders.TryAddWithoutValidation(
+                        "Authorization", "key=" + API_KEY);
+
+                    Task.WaitAll(client.PostAsync(url,
+                        new StringContent(jGcmData.ToString(), Encoding.Default, "application/json"))
+                            .ContinueWith(response =>
+                            {
+                                Console.WriteLine(response);
+                                Console.WriteLine("Message sent: check the client device notification tray.");
+                            }));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unable to send GCM message:");
+                Console.Error.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
---
+++ Revelaciones-Marianas/MainActivity.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Gms.Common;
+using Android.Util;
+
+namespace Revelaciones_Marianas
+{
+    [Activity(Label = "Revelaciones Marianas", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        TextView msgText;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+            msgText = FindViewById<TextView>(Resource.Id.msgText);
+
+            if (IsPlayServicesAvailable())
+            {
+                var intent = new Intent(this, typeof(RegistrationIntentService));
+                StartService(intent);
+            }
+        }
+
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    msgText.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+                else
+                {
+                    msgText.Text = "Sorry, this device is not supported";
+                    Finish();
+                }
+                return false;
+            }
+            else
+            {
+                msgText.Text = "Google Play Services is available.";
+                return true;
+            }
+        }
+    }
+}
+
